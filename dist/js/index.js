8fb9f56fcf9b58f911d71536b4f657e681060915
Goran P <87gorpri@skola.boras.se>
2023-12-19 15:11:59 +0100

    uppdatering av uppgift

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,6 +7,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const img_wrap = document.querySelector(".img-wrap");
+const BeerName = document.querySelector(".Beer-Name");
+function ShowInfo(data) {
+    console.log(data);
+}
 const beerURL = "https://api.punkapi.com/v2/beers/random";
 function getBeer() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -15,7 +20,15 @@ function getBeer() {
             console.log(response);
             if (response.status === 200) {
                 const data = yield response.json();
+                ShowInfo(data);
                 console.log(data);
+                data.forEach(element => {
+                    console.log(element.name);
+                    let img = document.createElement("img");
+                    img.setAttribute("src", element.image_url);
+                    img_wrap.appendChild(img);
+                    BeerName.innerHTML = element.name;
+                });
             }
             else {
                 throw Error('Något gick fel, försök igen senare');
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ interface Exempel {
     id: number,
     name: string,
     tagline: string,
-    image: string,
+    image_url: string | null,
     description: string,
     hops: string,
     AlcoholByVolume: string,
@@ -10,11 +10,18 @@ interface Exempel {
     Ingredients:string,
     FoodPairing:string,
     BrewersTips:string,
+}
 
+const img_wrap = document.querySelector(".img-wrap") as HTMLElement;
+const BeerName = document.querySelector(".Beer-Name") as HTMLElement;
 
+function ShowInfo(data:Exempel[]) 
+{
+    console.log(data);
 }
 
 const beerURL ="https://api.punkapi.com/v2/beers/random";
+
 
 async function getBeer() {
     try {
@@ -22,13 +29,23 @@ async function getBeer() {
         console.log(response);
         if (response.status === 200) {
             const data: Exempel[] = await response.json();
+            ShowInfo(data);
             console.log(data);
+            data.forEach(element => {
+                console.log(element.name);
+                let img = document.createElement("img");
+                img.setAttribute("src", element.image_url);
+                img_wrap.appendChild(img);
+                BeerName.innerHTML = element.name;
+            });
+    
         } else {
             throw Error('Något gick fel, försök igen senare');
         }
     } catch (error) {
         console.log(error);
     }
+    
 }
 
 getBeer(); 
